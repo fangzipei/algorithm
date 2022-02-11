a3915aed90765fa57f94f3cf117bb8b5a878bc9c
fangzipei <fangzipei@kaiwuren.com>
2022-02-11 16:50:52 +0800

    init feat: quick sort

---
+++ src/test/ZoneTest.java
@@ -0,0 +1,15 @@
+import com.zonefun.algorithm.sort.QuickSort;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ZoneTest {
+
+    @Test
+    public void testQuickSort(){
+        int[] nums = new int[]{2,54,1,5,6,74,2,124,634,6};
+        QuickSort.quickSortDesc(nums,0, nums.length-1);
+        Arrays.stream(nums).forEach(System.out::print);
+    }
+}
